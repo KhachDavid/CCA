f0b27fc036b867366b9fd3b9534e11823c4dd73f
David <khachatryandavid1@gmail.com>
2023-12-10 14:49:21 -0600

    Initial Commit

---
+++ ChooChooAuthority/ChooChooAuthorityApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChooChooAuthorityApp.swift
+//  ChooChooAuthority
+//
+//  Created by David Khachatryan on 12/10/23.
+//
+
+import SwiftUI
+
+@main
+struct ChooChooAuthorityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChooChooAuthority/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ChooChooAuthority
+//
+//  Created by David Khachatryan on 12/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
